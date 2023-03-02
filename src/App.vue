a7b390cd5069e91574b94e1ac8a255d186ba1136
JeongWoo <fixingwoo@gmail.com>
2023-03-02 13:45:39 +0900

    feat: 초기 컴포넌트 설계

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 <script>
-export default {};
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+
+export default {
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter,
+  },
+};
 </script>
 <style></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div><input type="text" /></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
